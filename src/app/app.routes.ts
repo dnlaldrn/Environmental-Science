9b400ea3541985ce9779a89f07d324f234cfbfa5
Daniel Manjares <danielaldreenm@gmail.com>
2026-03-17 22:27:07 +0800

    Add routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+     {
+    path: "",
+    loadComponent: () => import("./pages/home-page/home-page").then((x) => x.HomePage),
+  },
+    
+];
