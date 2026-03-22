10b52ae6a1eed69e5e47516087a1da59302fb71e
Daniel Manjares <danielaldreenm@gmail.com>
2026-03-22 12:44:23 +0800

    Add routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,5 +5,17 @@ export const routes: Routes = [
     path: "",
     loadComponent: () => import("./pages/home-page/home-page").then((x) => x.HomePage),
   },
+   {
+    path: "climate-change",
+    loadComponent: () => import("./pages/climate-change/climate-change").then((x) => x.ClimateChange),
+  },
+  {
+    path: "ozone",
+    loadComponent: () => import("./pages/ozone-shield/ozone-shield").then((x) => x.OzoneShield),
+  },
+   {
+    path: "global-warming",
+    loadComponent: () => import("./pages/global-warming/global-warming").then((x) => x.GlobalWarming),
+  },
     
 ];
