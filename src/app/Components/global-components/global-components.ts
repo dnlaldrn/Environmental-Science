a480b817156d636835df35b51a482d78e6aaa05e
Daniel Manjares <danielaldreenm@gmail.com>
2026-03-29 14:39:00 +0800

    Add global warming component

---
+++ src/app/Components/global-components/global-components.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-global-components',
+  imports: [],
+  templateUrl: './global-components.html',
+  styleUrl: './global-components.css',
+})
+export class GlobalComponents {}
