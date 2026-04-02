af1dd18a2b3ef096e87c83f81ad33d065341f049
Daniel Manjares <danielaldreenm@gmail.com>
2026-04-02 15:26:53 +0800

    Add click autoplay feature

--- src/app/Components/climate-component/climate-component.ts
+++ src/app/Components/climate-component/climate-component.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
+import { ɵEmptyOutletComponent } from "@angular/router";
+
 
 @Component({
   selector: 'app-climate-component',
-  imports: [],
+  imports: [ɵEmptyOutletComponent],
   templateUrl: './climate-component.html',
   styleUrl: './climate-component.css',
 })
-export class ClimateComponent {}
+export class ClimateComponent {
+  isVideo1playing = false;
+   isVideo2playing = false;
+
+
+  playVideo1(){
+    this.isVideo1playing = true;
+  
+  }
+  playVideo2(){
+     this.isVideo2playing = true;
+
+  }
+}
