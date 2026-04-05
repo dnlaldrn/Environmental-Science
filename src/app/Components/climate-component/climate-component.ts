c9ea9d9dc3e4972bf8135bd3a7570201434c3fdb
Daniel Manjares <danielaldreenm@gmail.com>
2026-04-05 23:55:34 +0800

    add interface

--- src/app/Components/climate-component/climate-component.ts
+++ src/app/Components/climate-component/climate-component.ts
@@ -1,24 +1,42 @@
 import { Component } from '@angular/core';
-import { ɵEmptyOutletComponent } from "@angular/router";
+import { RouterOutlet } from '@angular/router';
 
+interface Quizitems {
+  question: string;
+  choices: string[];
+  answer:string,
+}
 
 @Component({
   selector: 'app-climate-component',
-  imports: [ɵEmptyOutletComponent],
+  standalone: true,
+  imports: [RouterOutlet],
   templateUrl: './climate-component.html',
-  styleUrl: './climate-component.css',
+  styleUrls: ['./climate-component.css'],
 })
 export class ClimateComponent {
+
   isVideo1playing = false;
-   isVideo2playing = false;
+  isVideo2playing = false;
 
-
-  playVideo1(){
-    this.isVideo1playing = true;
-  
+  playVideo1() {
+    this.isVideo1playing = !this.isVideo1playing;
   }
-  playVideo2(){
-     this.isVideo2playing = true;
 
+  playVideo2() {
+    this.isVideo2playing = !this.isVideo2playing;
   }
+
+  quizItems: Quizitems[] = [
+    {
+      question: 'It is the state of the atmosphere at a particular place at a particular moment',
+      choices: [
+        'Weather',
+        'Climate',
+        'Temperature',
+        'Atmosphere'
+      ],
+      answer:"Weather",
+    }
+  ];
 }
