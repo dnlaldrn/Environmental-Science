e89481983771e8a8d4cdb66b28815f15654512f0
Daniel Manjares <danielaldreenm@gmail.com>
2026-04-10 15:52:16 +0800

    Add quiz component

--- src/app/pages/home-page/home-page.ts
+++ src/app/pages/home-page/home-page.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import {QuizComponent} from '../../Components/quiz-component/quiz-component'
 
 @Component({
   selector: 'app-home-page',
-  imports: [RouterLink],
+  imports: [RouterLink, QuizComponent],
   templateUrl: './home-page.html',
   styleUrl: './home-page.css',
 })
